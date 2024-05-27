9a1dd1815623712e953b34fa55315e589b96753b
jadoredarina <meizu5@ukr.net>
2024-05-27 16:03:35 -0400

    Nabir sliv unikalni

---
+++ src/main/java/nabirslov/unikalni.java
@@ -0,0 +1,27 @@
+package nabirslov;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Arrays;
+
+public class unikalni {
+
+    public static void main(String[] args) {
+        processString("масло, масло, ковбаса, молоко");
+    }
+
+    public static void processString(String input) {
+        String[] words = input.split("[,\\s]+"); // [\\s,]+ - любой пробел или запятая
+
+        Set<String> uniqueWords = new HashSet<>(Arrays.asList(words));
+
+        Set<String> result = new HashSet<>();
+        for (String word : uniqueWords) {
+            result.add(word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase());
+        }
+
+        for (String word : result) {
+            System.out.println(word);
+        }
+    }
+}
